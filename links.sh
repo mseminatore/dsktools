5ace2790b8beb6c2afb3f6234147dd577d719742
Mark Seminatore <mseminatore@users.noreply.github.com>
2024-05-13 12:05:37 -0700

    add script for links

---
+++ links.sh
@@ -0,0 +1,12 @@
+# /usr/bin/bash
+
+DSKPATH=$(dirname $0)
+cd "$DSKPATH"
+DSKPATH=$(pwd)
+
+cd /usr/local/bin
+
+ln -sf "$DSKPATH/dsktools" dsktools
+ln -sf "$DSKPATH/dsk_new" dsk_new
+ln -sf "$DSKPATH/dsk_add" dsk_new
+ln -sf "$DSKPATH/dsk_del" dsk_del
